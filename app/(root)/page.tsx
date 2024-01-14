e7773dc8108f854f29764f9cd0339c7504a3460e
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-01-14 11:56:31 +0100

    feat: Add styling for Heading in Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Headings from '@/components/Atoms/Headings/Headings';
 export default function Home() {
   return (
     <>
-      <Headings level={1}>
+      <Headings
+        level={1}
+        customClass="head-text text-left"
+      >
         <>Home</>
       </Headings>
     </>
