dd0133d347c0bfe3407aa1e8cfb5226f1dbe8919
Adrian <adrian.ciolkiewicz@makimo.pl>
2024-01-19 21:26:57 +0100

    feat: Print information when posts were not found

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from '@clerk/nextjs';
 
 import Headings from '@/components/Atoms/Headings/Headings';
+import Typography from '@/components/Atoms/Typography/Typography';
 import { fetchPosts } from '@/lib/actions/thread.actions';
 
 export default async function Home() {
@@ -18,19 +19,27 @@ export default async function Home() {
       </Headings>
 
       <section className="mt-9 flex flex-col gap-10">
-        {fetchedPosts.posts.map((post) => (
-          <ThreadCard
-            key={post._id}
-            id={post._id}
-            currentUserId={user?.id}
-            parentId={post.parentId}
-            content={post.text}
-            author={post.author}
-            community={post.community}
-            createdAt={post.createdAt}
-            comments={post.children}
-          />
-        ))}
+        {fetchedPosts.posts.length === 0 ? (
+          <Typography customClass="no-result">
+            <>No threads found.</>
+          </Typography>
+        ) : (
+          <>
+            {fetchedPosts.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
       </section>
     </>
   );
